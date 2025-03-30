705602d27fdaee87b7ea51973ac6a9732cb77220
vickvey <vickvey0303@gmail.com>
2025-03-30 23:53:26 +0530

    added JIOFIN data and structure upgrade

---
+++ src/models.py
@@ -0,0 +1,14 @@
+class Stock:
+    def __init__(self, exp_return, risk):
+        self.exp_return = exp_return
+        self.risk = risk
+    
+class Portfolio:
+    def __init__():
+        pass
+
+class Engine:
+    def __init__():
+        pass
+    
+    
